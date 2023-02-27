8001b36751f185d4cb2954ebde1b59a574070ce5
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-02-27 14:37:22 -0600

    Initial Commit

---
+++ ScalesAndChords/ScalesAndChordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalesAndChordsApp.swift
+//  ScalesAndChords
+//
+//  Created by Renato Oliveira Fraga on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ScalesAndChordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
